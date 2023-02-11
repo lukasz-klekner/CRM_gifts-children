f5a0eaef06d5184dbec2c0156288a60396770922
Luki Klekner <lukaszklekner@gmail.com>
2023-02-11 17:17:32 +0100

    feat: add the list of gifts to children

--- routes/child.js
+++ routes/child.js
@@ -1,12 +1,16 @@
 const { Router } = require("express");
 const { ChildRecord } = require("../records/childRecord");
+const { GiftRecord } = require("../records/giftRecord");
 
 const childRouter = Router()
 
 childRouter.get('/', (req, res) =>{
     const childrenList = ChildRecord.listAll()
+    const giftsList = GiftRecord.listAll()
+
     res.render('children/list', {
-        childrenList
+        childrenList,
+        giftsList
     })
 })
 
