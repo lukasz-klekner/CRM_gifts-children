35aa7812382f8bfb726c1020bd1a9ed5b01aff36
Luki Klekner <lukaszklekner@gmail.com>
2023-02-11 12:53:37 +0100

    feat: initial set up of express

---
+++ index.js
@@ -0,0 +1,29 @@
+const { urlencoded, static, json } = require('express')
+const express = require('express')
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
+
+const app = express()
+
+app.use(methodOverride('_method'))
+app.use(urlencoded({
+    extended: true,
+}))
+app.use(static('public'))
+// app.use(json()) Content-Type: application/json
+
+app.engine('.hbs', engine({ 
+    extname: '.hbs',
+    // helpers: handlebarsHelpers
+}))
+app.set('view engine', '.hbs')
+
+app.get('/', (req, res) =>{
+    res.send('Hello, world')
+})
+
+// app.use(handleError)
+
+app.listen(3000, 'localhost', () => {
+    console.log('listening on port 3000')
+})
