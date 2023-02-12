8ce67a9d6a721e2984c51dabe54c38fa048f2e1f
Luki Klekner <lukaszklekner@gmail.com>
2023-02-12 12:27:47 +0100

    feat: add insert method and validation to gifRecord

--- records/giftRecord.js
+++ records/giftRecord.js
@@ -1,6 +1,26 @@
 const { giftsCollection } = require("../utils/db")
+const { ValidationError } = require("../utils/errors")
 
 class GiftRecord {
+    constructor(object){
+        if(!object.name || object.name.length < 3 || object.name.length > 50){
+            throw new ValidationError('Nazwa prezentu musi miec od 3 do 50 znakow!')
+        }
+
+        if(!object.amount || object.amount < 1){
+            throw new ValidationError('Liczba sztuk prezentu musi byc wieksza od 1!')
+        }
+
+        this.name = object.name
+        this.amount = object.amount
+    }
+
+    async insert(){
+        await giftsCollection.insertOne(this)
+
+        return this.id
+    }
+
     static async listAll(){
         return await giftsCollection.find().toArray()
     }
